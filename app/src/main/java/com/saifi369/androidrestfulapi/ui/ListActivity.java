99bbc03b1803325134e9a99add0a791b2410c514
M Saif Ullah <chsaif369@gmail.com>
2018-11-16 21:03:30 +0500

    Download Images and Show in Recycler View

--- app/src/main/java/com/saifi369/androidrestfulapi/ui/ListActivity.java
+++ app/src/main/java/com/saifi369/androidrestfulapi/ui/ListActivity.java
@@ -70,6 +70,7 @@ public class ListActivity extends AppCompatActivity implements LoaderManager.Loa
         }
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
