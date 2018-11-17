8880470ca3c1d0308aec0122ca7b5f6cd7f224b4
M Saif Ullah <chsaif369@gmail.com>
2018-11-17 19:55:22 +0500

    Downloading images with picasso library(Auto Cache)

--- app/src/main/java/com/saifi369/androidrestfulapi/adapters/MyDataAdapter.java
+++ app/src/main/java/com/saifi369/androidrestfulapi/adapters/MyDataAdapter.java
@@ -2,8 +2,6 @@ package com.saifi369.androidrestfulapi.adapters;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -15,11 +13,8 @@ import android.widget.TextView;
 
 import com.saifi369.androidrestfulapi.R;
 import com.saifi369.androidrestfulapi.model.CityItem;
-import com.saifi369.androidrestfulapi.utils.CacheImageManager;
+import com.squareup.picasso.Picasso;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -56,21 +51,28 @@ public class MyDataAdapter extends RecyclerView.Adapter<MyDataAdapter.MyViewHold
         Log.d(TAG, "onBindViewHolder: Name: "+cityDataItem.getCityname()+" : "+cityDataItem.getImage());
 
         holder.textView.setText(cityDataItem.getCityname());
+        String PHOTO_BASE_URL=
+                "http://10.0.2.2/pakinfo/images/"+cityDataItem.getImage();
 
-        try{
-            Bitmap bitmap=CacheImageManager.getImage(mContext,cityDataItem);
+        Picasso.get()
+                .load(PHOTO_BASE_URL)
+                .into(holder.imageView);
 
-            if (bitmap == null) {
-                MyImageTask task=new MyImageTask();
-                task.setViewHolder(holder);
-                task.execute(cityDataItem);
-            }else {
-                holder.imageView.setImageBitmap(bitmap);
-            }
 
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+//        try{
+//            Bitmap bitmap=CacheImageManager.getImage(mContext,cityDataItem);
+//
+//            if (bitmap == null) {
+//                MyImageTask task=new MyImageTask();
+//                task.setViewHolder(holder);
+//                task.execute(cityDataItem);
+//            }else {
+//                holder.imageView.setImageBitmap(bitmap);
+//            }
+//
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
 
 
 //        InputStream inputStream=null;
@@ -111,59 +113,59 @@ public class MyDataAdapter extends RecyclerView.Adapter<MyDataAdapter.MyViewHold
         }
     }
 
-    class MyImageTask extends AsyncTask<CityItem,Void,Bitmap>{
-
-        private static final String PHOTO_BASE_URL=
-                "http://10.0.2.2/pakinfo/images/";
-        private CityItem mCityItem;
-        private MyViewHolder mViewHolder;
-
-        public void setViewHolder(MyViewHolder myViewHolder){
-            this.mViewHolder=myViewHolder;
-        }
-
-        @Override
-        protected Bitmap doInBackground(CityItem... cityItems) {
-            Bitmap bitmap = null;
-            mCityItem=cityItems[0];
-
-            String imageurl=PHOTO_BASE_URL+mCityItem.getImage();
-
-            InputStream inputStream=null;
-
-            try {
-                URL imageUrl=new URL(imageurl);
-                inputStream = (InputStream) imageUrl.getContent();
-                bitmap=BitmapFactory.decodeStream(inputStream);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-                if (inputStream != null) {
-                    try {
-                        inputStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            int sleepTime=random.nextInt(400);
-            try {
-                Thread.sleep(sleepTime);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            Log.d(TAG, "doInBackground: Image downloaded: "+imageurl);
-
-            return bitmap;
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
-            mViewHolder.imageView.setImageBitmap(bitmap);
-            CacheImageManager.putImage(mContext,mCityItem,bitmap);
-        }
-    }
+//    class MyImageTask extends AsyncTask<CityItem,Void,Bitmap>{
+//
+//        private static final String PHOTO_BASE_URL=
+//                "http://10.0.2.2/pakinfo/images/";
+//        private CityItem mCityItem;
+//        private MyViewHolder mViewHolder;
+//
+//        public void setViewHolder(MyViewHolder myViewHolder){
+//            this.mViewHolder=myViewHolder;
+//        }
+//
+//        @Override
+//        protected Bitmap doInBackground(CityItem... cityItems) {
+//            Bitmap bitmap = null;
+//            mCityItem=cityItems[0];
+//
+//            String imageurl=PHOTO_BASE_URL+mCityItem.getImage();
+//
+//            InputStream inputStream=null;
+//
+//            try {
+//                URL imageUrl=new URL(imageurl);
+//                inputStream = (InputStream) imageUrl.getContent();
+//                bitmap=BitmapFactory.decodeStream(inputStream);
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            finally {
+//                if (inputStream != null) {
+//                    try {
+//                        inputStream.close();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//            int sleepTime=random.nextInt(400);
+//            try {
+//                Thread.sleep(sleepTime);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            Log.d(TAG, "doInBackground: Image downloaded: "+imageurl);
+//
+//            return bitmap;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Bitmap bitmap) {
+//            super.onPostExecute(bitmap);
+//            mViewHolder.imageView.setImageBitmap(bitmap);
+//            CacheImageManager.putImage(mContext,mCityItem,bitmap);
+//        }
+//    }
 }
